e11795d7f40c836eeb3a92b6004f1f7fdfdaf243
Andreas Sandskär <ekrarna@gmail.com>
2018-11-23 16:05:23 +0100

    skapat en ny array

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ var app = angular
             .get("https://swapi.co/api/people")
             .then(function (adressen) {
                 // people
-                console.log(adressen)
-                $scope.people = adressen.data.results[0];
+                //    console.log(adressen)
+                $scope.people = adressen.data.results
+                // nödlösning med extra variabel
+                var folk = []
+
+                $scope.people.forEach(element => {
+                    console.log(element.name)
+                    $scope.folk = folk.push(element.name)
+                });
             });
     });
 
