18c043430b580b810077b8bb569b742b36d53656
Andreas Sandskär <ekrarna@gmail.com>
2018-11-23 15:32:32 +0100

    visa ett namn

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var app = angular.module('starWars', ['ngRoute']);
-
-
-
-
+var app = angular
+    .module('starWars', []) // 'ngRoute'
+    .controller("controller", function ($scope, $http) {
+        $http
+            .get("https://swapi.co/api/people")
+            .then(function (adressen) {
+                // people
+                console.log(adressen)
+                $scope.people = adressen.data.results[0];
+            });
+    });
 
